ae8178941ea1dd8067afbc541a43b6bb17a0c30f
gaute <gaute.magnussen@gmail.com>
2010-10-20 20:24:41 +0200

    Had some problems with git. and had to do some stuff a few times

--- Sample/CQRSCode/ReadModel/Handlers/InventoryListView.cs
+++ Sample/CQRSCode/ReadModel/Handlers/InventoryListView.cs
@@ -9,18 +9,18 @@ namespace CQRSCode.ReadModel.Handlers
     {
         public void Handle(InventoryItemCreated message)
         {
-            BullShitDatabase.List.Add(new InventoryItemListDto(message.Id, message.Name));
+            InMemoryDatabase.List.Add(new InventoryItemListDto(message.Id, message.Name));
         }
 
         public void Handle(InventoryItemRenamed message)
         {
-            var item = BullShitDatabase.List.Find(x => x.Id == message.Id);
+            var item = InMemoryDatabase.List.Find(x => x.Id == message.Id);
             item.Name = message.NewName;
         }
 
         public void Handle(InventoryItemDeactivated message)
         {
-            BullShitDatabase.List.RemoveAll(x => x.Id == message.Id);
+            InMemoryDatabase.List.RemoveAll(x => x.Id == message.Id);
         }
     }
 }
