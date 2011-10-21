81cd457a0806ec90dc4442337b07d0e5d5ed292e
Gaute Magnussen <gaute.magnussen@gmail.com>
2011-10-21 14:05:53 +0200

    Fixed typo

--- Framework/CQRSlite.Tests/DomainTests/When_adding_aggregates_to_repository.cs
+++ Framework/CQRSlite.Tests/DomainTests/When_adding_aggregates_to_repository.cs
@@ -8,7 +8,7 @@ namespace CQRSlite.Tests.DomainTests
     [TestFixture]
     public class When_adding_aggregates_to_repository
     {
-        private Repository<TestSnapshotAggreagate> _rep;
+        private Repository<TestSnapshotAggregate> _rep;
 
         [SetUp]
         public void SetUp()
@@ -16,14 +16,14 @@ namespace CQRSlite.Tests.DomainTests
             var eventStore = new TestEventStore();
             var eventPublisher = new TestEventPublisher();
             var snapshotStore = new NullSnapshotStore();
-            _rep = new Repository<TestSnapshotAggreagate>(eventStore, snapshotStore, eventPublisher);
+            _rep = new Repository<TestSnapshotAggregate>(eventStore, snapshotStore, eventPublisher);
         }
 
         [Test]
         public void Should_throw_if_different_object_with_tracked_guid_is_added()
         {
-            var aggregate = new TestSnapshotAggreagate();
-            var aggregate2 = new TestSnapshotAggreagate();
+            var aggregate = new TestSnapshotAggregate();
+            var aggregate2 = new TestSnapshotAggregate();
             aggregate2.SetId(aggregate.Id);
             _rep.Add(aggregate);
             Assert.Throws<TrackedAggregateAddedException>(() => _rep.Add(aggregate2));
@@ -32,7 +32,7 @@ namespace CQRSlite.Tests.DomainTests
         [Test]
         public void Should_not_throw_if_object_already_tracked()
         {
-            var aggregate = new TestSnapshotAggreagate();
+            var aggregate = new TestSnapshotAggregate();
             _rep.Add(aggregate);
             _rep.Add(aggregate);
         }
