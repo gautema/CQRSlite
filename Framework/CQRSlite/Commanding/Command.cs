c090d752d586c1b7e17c51d6fd1f2dc52a620e00
Gaute Magnussen <gaute.magnussen@gmail.com>
2011-10-28 23:30:15 +0200

    Added Id to command.

--- Framework/CQRSlite/Commanding/Command.cs
+++ Framework/CQRSlite/Commanding/Command.cs
@@ -1,7 +1,10 @@
-﻿namespace CQRSlite.Commanding
+﻿using System;
+
+namespace CQRSlite.Commanding
 {
     public class Command : Message
     {
+        public Guid Id { get; set; }
         public int ExpectedVersion { get; set; }
     }
 }
