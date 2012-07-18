d92a60accf6c8d560906a55fb319f4084df645e5
Gaute Magnussen <gaute.magnussen@gmail.com>
2012-07-18 16:31:19 +0200

    Added a new test for existing untested functionality

--- Framework/CQRSlite.Tests/TestSubstitutes/TestInMemoryEventStore.cs
+++ Framework/CQRSlite.Tests/TestSubstitutes/TestInMemoryEventStore.cs
@@ -21,7 +21,7 @@ namespace CQRSlite.Tests.TestSubstitutes
 
         public int GetVersion(Guid aggregateId)
         {
-            if (_list.Count() == 0) return 0;
+            if (!_list.Any()) return 0;
             return _list.Max(x => x.Version);
         }
     }
