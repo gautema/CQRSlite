883a98b9ded9727114b77830a3d4186bc9e03b09
Gaute Magnussen <gaute.magnussen@gmail.com>
2012-10-19 12:13:59 +0200

    Made cache lock on aggregate and not global.

--- Framework/CQRSlite/Cache/CacheRepository.cs
+++ Framework/CQRSlite/Cache/CacheRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Runtime.Caching;
 using CQRSlite.Domain;
@@ -13,34 +14,40 @@ namespace CQRSlite.Cache
         private readonly IEventStore _eventStore;
         private readonly MemoryCache _cache;
         private readonly Func<CacheItemPolicy> _policyFactory;
+        private static readonly ConcurrentDictionary<string, object> _locks = new ConcurrentDictionary<string, object>();
 
         public CacheRepository(IRepository repository, IEventStore eventStore)
         {
             _repository = repository;
             _eventStore = eventStore;
             _cache = MemoryCache.Default;
-            _policyFactory = () => new CacheItemPolicy {SlidingExpiration = new TimeSpan(0, 1, 0, 0)};
+            _policyFactory = () => new CacheItemPolicy { SlidingExpiration = new TimeSpan(0, 1, 0, 0), RemovedCallback = x =>
+                                                                                                                             {
+                                                                                                                                 object o;
+                                                                                                                                 _locks.TryRemove(x.CacheItem.Key, out o);
+                                                                                                                             } };
         }
 
         public void Save<T>(T aggregate, int? expectedVersion = null) where T : AggregateRoot
         {
-            lock (_policyFactory)
+            var idstring = aggregate.Id.ToString();
+            lock (_locks.GetOrAdd(idstring, _ => new object()))
             {
                 if (!IsTracked(aggregate.Id))
-                    _cache.Add(aggregate.Id.ToString(), aggregate, _policyFactory.Invoke());
-
+                    _cache.Add(idstring, aggregate, _policyFactory.Invoke());
                 _repository.Save(aggregate, expectedVersion);
             }
         }
 
         public T Get<T>(Guid aggregateId) where T : AggregateRoot
         {
-            lock (_policyFactory)
+            var idstring = aggregateId.ToString();
+            lock (_locks.GetOrAdd(idstring, _ => new object()))
             {
                 T aggregate;
                 if (IsTracked(aggregateId))
                 {
-                    aggregate = (T) _cache.Get(aggregateId.ToString());
+                    aggregate = (T)_cache.Get(idstring);
                     var events = _eventStore.Get(aggregateId, aggregate.Version);
                     if (events.Any() && events.First().Version != aggregate.Version + 1)
                         throw new EventsOutOfOrderException();
@@ -51,7 +58,6 @@ namespace CQRSlite.Cache
 
                 aggregate = _repository.Get<T>(aggregateId);
                 _cache.Add(aggregateId.ToString(), aggregate, _policyFactory.Invoke());
-
                 return aggregate;
             }
         }
