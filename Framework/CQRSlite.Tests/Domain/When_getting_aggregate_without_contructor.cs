31c93a8075039346aa267661c7e377dcf8491f0a
Gaute Magnussen <gaute.magnussen@gmail.com>
2016-05-27 15:37:36 +0200

    Fixed broken test

--- Framework/CQRSlite.Tests/Domain/When_getting_aggregate_without_contructor.cs
+++ Framework/CQRSlite.Tests/Domain/When_getting_aggregate_without_contructor.cs
@@ -9,19 +9,24 @@ namespace CQRSlite.Tests.Domain
 	[TestFixture]
     public class When_getting_aggregate_without_contructor
     {
-	    private ISession _session;
+	    private Guid _id;
+	    private Repository _repository;
 
 	    [SetUp]
         public void Setup()
-        {
+	    {
+	        _id = Guid.NewGuid();
             var eventStore = new TestInMemoryEventStore();
-            _session = new Session(new Repository(eventStore));
-        }
+            _repository = new Repository(eventStore);
+	        var aggreagate = new TestAggregateNoParameterLessConstructor(1, _id);
+            aggreagate.DoSomething();
+            _repository.Save(aggreagate);
+	    }
 
         [Test]
         public void Should_throw_missing_parameterless_constructor_exception()
         {
-            Assert.Throws<MissingParameterLessConstructorException>(() => _session.Get<TestAggregateNoParameterLessConstructor>(Guid.NewGuid()));
+            Assert.Throws<MissingParameterLessConstructorException>(() => _repository.Get<TestAggregateNoParameterLessConstructor>(_id));
         }
     }
 }
