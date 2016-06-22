566fc9aa44173f5544261bc2d7464464abea8a90
Andreas <larsson_andreas@live.se>
2016-06-22 13:29:01 +0200

    renamed CQRSlite to CQRSlite.NetCore

--- src/CQRSlite.NetCore/Properties/AssemblyInfo.cs
+++ src/CQRSlite.NetCore/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -7,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CQRSlite")]
+[assembly: AssemblyProduct("CQRSlite.NetCore")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
