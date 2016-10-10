0d0eed5af5a55af1b976b9df753634472aaa5d25
Gaute Magnussen <gaute.magnussen@gmail.com>
2016-10-10 11:08:32 +0200

    Working with .NET Standard 1.3

--- Framework/CQRSlite/Domain/Factories/AggregateFactory.cs
+++ Framework/CQRSlite/Domain/Factories/AggregateFactory.cs
@@ -9,7 +9,11 @@ namespace CQRSlite.Domain.Factories
         {
             try
             {
+#if NETSTANDARD1_3
+                return (T)Activator.CreateInstance(typeof(T));
+#else
                 return (T)Activator.CreateInstance(typeof(T), true);
+#endif
             }
             catch (MissingMethodException)
             {
