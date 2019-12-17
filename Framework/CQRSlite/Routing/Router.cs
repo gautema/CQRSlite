099e8d07fd313b5477e9f6a69b96704dbf3a5f68
kgorlachev <kgorlachev@integrio.net>
2019-12-17 14:26:33 +0200

    Routes marked protected for the sake of sake Router extensibility

--- Framework/CQRSlite/Routing/Router.cs
+++ Framework/CQRSlite/Routing/Router.cs
@@ -14,9 +14,9 @@ namespace CQRSlite.Routing
     /// </summary>
     public class Router : ICommandSender, IEventPublisher, IQueryProcessor, IHandlerRegistrar
     {
-        private readonly Dictionary<Type, List<Func<IMessage, CancellationToken, Task>>> _routes = new Dictionary<Type, List<Func<IMessage, CancellationToken, Task>>>();
+        protected readonly Dictionary<Type, List<Func<IMessage, CancellationToken, Task>>> _routes = new Dictionary<Type, List<Func<IMessage, CancellationToken, Task>>>();
 
-        public void RegisterHandler<T>(Func<T, CancellationToken, Task> handler) where T : class, IMessage
+        public virtual void RegisterHandler<T>(Func<T, CancellationToken, Task> handler) where T : class, IMessage
         {
             if (!_routes.TryGetValue(typeof(T), out var handlers))
             {
@@ -26,7 +26,7 @@ namespace CQRSlite.Routing
             handlers.Add((message, token) => handler((T)message, token));
         }
 
-        public Task Send<T>(T command, CancellationToken cancellationToken = default) where T : class, ICommand
+        public virtual Task Send<T>(T command, CancellationToken cancellationToken = default) where T : class, ICommand
         {
             var type = command.GetType();
             if (!_routes.TryGetValue(type, out var handlers))
@@ -36,7 +36,7 @@ namespace CQRSlite.Routing
             return handlers[0](command, cancellationToken);
         }
 
-        public Task Publish<T>(T @event, CancellationToken cancellationToken = default) where T : class, IEvent
+        public virtual Task Publish<T>(T @event, CancellationToken cancellationToken = default) where T : class, IEvent
         {
             if (!_routes.TryGetValue(@event.GetType(), out var handlers))
                 return Task.FromResult(0);
@@ -49,7 +49,7 @@ namespace CQRSlite.Routing
             return Task.WhenAll(tasks);
         }
 
-        public Task<TResponse> Query<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken = default)
+        public virtual Task<TResponse> Query<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken = default)
         {
             var type = query.GetType();
             if (!_routes.TryGetValue(type, out var handlers))
