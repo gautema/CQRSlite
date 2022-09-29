23e89dc705ce756c6be46007d9079ff7216fa3fa
Arve Systad <arvesystad@live.com>
2022-09-29 21:16:15 +0200

    Improve error message in EventIdIncorrectException to aid in debugging

--- Framework/CQRSlite/Domain/Exception/EventIdIncorrectException.cs
+++ Framework/CQRSlite/Domain/Exception/EventIdIncorrectException.cs
@@ -4,8 +4,8 @@ namespace CQRSlite.Domain.Exception
 {
     public class EventIdIncorrectException : System.Exception
     {
-        public EventIdIncorrectException( Guid id, Guid aggregateId)
-            : base($"Event {id} has a different Id from it's aggregates Id ({aggregateId})")
+        public EventIdIncorrectException( Guid id, Guid aggregateId, Type eventType)
+            : base($"Event {id} of type {eventType.Name} has a different Id from it's aggregates Id ({aggregateId}). Have you have forgotten to set the Id property on the event class?")
         { }
     }
 }
